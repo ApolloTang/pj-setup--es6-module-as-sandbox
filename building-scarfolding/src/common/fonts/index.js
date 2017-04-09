40bb16777ac31a58fba79e4b7ea571ea202179a6
Apollo Tang <apollotang@lights.local>
2017-04-09 14:01:32 -0400

    clean up; turn off global styling

--- building-scarfolding/src/common/fonts/index.js
+++ building-scarfolding/src/common/fonts/index.js
@@ -2,7 +2,7 @@ if (process && process.env && process.env.CONSOLE_LOG) {
     console.info('log from file: src/common/fonts/index.js'); // eslint-disable-line no-console
 }
 
-// require('./roboto/index.js');
+require('./roboto/index.js');
 
 
 
